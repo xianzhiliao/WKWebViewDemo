ce3cc614becb21ae60300f33fb9c8da34078e979
廖贤志 <lxz@putao.cn>
2017-02-06 18:14:20 +0800

    init

--- WKWebViewDemo/ViewController.m
+++ WKWebViewDemo/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+@import WebKit;
 
 @interface ViewController ()
+<
+WKNavigationDelegate,
+WKUIDelegate,
+WKScriptMessageHandler
+>
+
+@property (nonatomic, strong) WKWebView *webView;
 
 @end
 
@@ -17,12 +25,80 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    //1. JS调用OC 添加处理脚本
+    [config.userContentController addScriptMessageHandler:self name:@"showMobile"];
+    config.preferences.minimumFontSize = 18;
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:config];
+    self.webView.allowsBackForwardNavigationGestures = YES;
+    self.webView.navigationDelegate = self;
+    self.webView.UIDelegate = self;
+    [self.view addSubview:self.webView];
+    NSURL *urlLocalH5 = [[NSBundle mainBundle]URLForResource:@"index" withExtension:@"html"];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:urlLocalH5]];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - WKNavigationDelegate
+
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"didStartProvisionalNavigation");
+}
+// 当内容开始返回时调用
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    NSLog(@"didCommitNavigation");
+}
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSLog(@"didFinishNavigation");
+}
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"didFailProvisionalNavigation");
+}
+
+// 接收到服务器跳转请求之后调用
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation{
+}
+// 在收到响应后，决定是否跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+// 在发送请求之前，决定是否跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+#pragma mark - WKUIDelegate
+
+// 创建一个新的WebView
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures{
+    return nil;
+}
+
+/**
+ *  web界面中有弹出警告框时调用
+ *
+ *  @param webView           实现该代理的webview
+ *  @param message           警告框中的内容
+ *  @param frame             主窗口
+ *  @param completionHandler 警告框消失调用
+ */
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
+    NSLog(@"alert");
+    completionHandler();
+}
+
+#pragma mark - WKScriptMessageHandler
+
+// 从web界面中接收到一个脚本时调用
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
+    NSLog(@"messageName:%@\nmessageBody:%@", message.name,message.body);
 }
 
 
